ab3f37ec80983db33ef6c12a36aa845b0a71456f
sinthiya04 <sinthiya@drylogics.com>
2020-07-09 10:46:36 +0530

    adding my new infixtopostfix ruby program

--- infixtopostfix.rb
+++ infixtopostfix.rb
@@ -1,37 +1,38 @@
 def infix_postfix(str)
-  @precidence = {
-    "*" => 2,
-    "/" => 2,
-    "+" => 1,
-    "-" => 1,
-  }
-  output = []
-  stack = []
-  expression = str.split(" ")
-expression.each do | char |
-  if char = str
-    output << char
-  elsif stack.empty?
-        stack << char
-  elsif stack.last == char
-        stack << char
-  elsif @precidence[stack.last] < @precidence[char]
-        stack << char
-  elsif @precidence[stack.last] > @precidence[char]
-        count = 0
-      stack.each do | s |
-        if @precidence[s] > @precidence[char]
+  operators = ["+","-","/","*"]
+    @precidence = {
+      "*" => 2,
+      "/" => 2,
+      "+" => 1,
+      "-" => 1,
+    }
+    output = []
+    stack = []
+    expression = str.split(" ")
+  expression.each do | char |
+    if !(operators.include? (char))
+      output.push(char)
+   elsif stack.empty?
+         stack << char
+       elsif @precidence[stack.last] == @precidence[char]
+          stack << char
+   elsif @precidence[stack.last] < @precidence[char]
+          stack << char
+   elsif @precidence[stack.last] > @precidence[char]
+          count = 0
+        stack.each do | s |
+          if @precidence[s] > @precidence[char]
           output << s
-          count += 1
+           count += 1
+         end
         end
-      end
-        stack.pop(count)
-        stack << char
-  end
-end
-    until stack.empty?
-      output << stack.pop
+          stack.pop(count)
+          stack << char
     end
-  return output.join
+  end
+      until stack.empty?
+        output << stack.pop
+      end
+    return output.join
 end
-p infix_postfix("a + b")
+
